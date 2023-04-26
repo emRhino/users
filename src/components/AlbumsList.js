ad0768960cc7f361164f3b1e8da9d261cb67c45b
Marcin S <you@example.com>
2023-04-26 15:03:00 +0200

    Query

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,12 +1,42 @@
-import { useFetchAlbumsQuery } from '../store';
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
-  const results = useFetchAlbumsQuery(user);
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [doAddAlbum, isAddingAlbum, addingAlbumError] = useAddAlbumMutation();
 
-  console.log(results);
   // console.log(data, error, isLoading);
 
-  return <div>Albums for {user.name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (error) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((album) => {
+      const header = album.title;
+
+      return (
+        <ExpandablePanel key={album.id} title={album.title}>
+          {header}
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  const handleAddAlbum = () => {
+    doAddAlbum(user);
+  };
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <Button onClick={handleAddAlbum}>Add Album</Button>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
